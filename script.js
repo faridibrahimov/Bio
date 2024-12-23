e24e3109a95a0e5ea80000ce96878a6bb017751d
faridibrahimov <83863119+faridibrahimov@users.noreply.github.com>
2024-12-23 21:30:02 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+$(document).ready(function () {
+    $('.slider').bxSlider();
+});
+
+let links = document.querySelectorAll('.headera');
+    let targetID;
+    links.forEach(function (element) {
+        element.addEventListener('click', function (event) {
+            event.preventDefault();
+            targetID = element.getAttribute('href');
+            
+            document.querySelector(targetID).scrollIntoView({
+                behavior: 'smooth',
+                block: 'start'
+            })
+        })
+    })
